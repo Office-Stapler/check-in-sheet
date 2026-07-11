6593c184a7dd9339a884df8b3ef96582e3266608
Raymond Lam <53181007+Office-Stapler@users.noreply.github.com>
2026-07-11 17:10:53 +1000

    update deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/check-in-sheet/',
+  base: 'https://office-stapler.github.io/check-in-sheet/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
